b53ce12d937591cfc12efceb965ba953eeb68d52
hyun0k <hyunyoung9310@gmail.com>
2021-04-16 18:11:01 +0900

    Refactor:메인 페이지 메뉴바 데이터 전달 엔드포인트 수정 - 중첩 리스트가 아닌 각 카테고리 별로 별도의 리스트로 전달하도록 구현 - model 각각 import 하도록 수정

--- products/views.py
+++ products/views.py
@@ -1,39 +1,52 @@
 from django.http  import JsonResponse
 from django.views import View
 
-from .models      import *
+from .models      import Menu, MainCategory, SubCategory
 
 class MenuView(View):
     def get(self, request):
         try:
-            menu_list = Menu.objects.all()
             results = []
+            
+            menu_list = Menu.objects.all()
+            menu_items = []
             for menu in menu_list:
-                main_category = menu.maincategory_set.all()
-                main_list = []
-                for main in main_category:
-                    sub_category = main.subcategory_set.all()
-                    sub_list = []
-                    for sub in sub_category:
-                        sub_pair = {"id" : sub.id, "name" : sub.name}
-                        sub_list.append(sub_pair)
-                    main_list.append(
-                        {   
-                            "id"           : main.id,
-                            "name"         : main.name,
-                            "sub_category" : sub_list
-                        }
-                    )
-                results.append(
+                menu_items.append(
                     {
-                        "id"            : menu.id,
-                        "name"          : menu.name,
-                        "main_category" : main_list
+                        "id" : menu.id,
+                        "name" : menu.name
                     }
                 )
+            
+            main_list = MainCategory.objects.all()
+            main_items = []
+            for main in main_list:
+                main_items.append(
+                    {
+                        "id" : main.id,
+                        "name" : main.name
+                    }
+                )
+            
+            sub_list = SubCategory.objects.all()
+            sub_items = []
+            for sub in sub_list:
+                sub_items.append(
+                    {
+                        "id" : sub.id,
+                        "name" : sub.name
+                    }
+                )
+            
+            results.append(
+                {
+                    "menu"          : menu_items,
+                    "main_category" : main_items,
+                    "sub_category"  : sub_items
+                }
+            )
+
             return JsonResponse({'results':results}, status=200)
         
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
-
-

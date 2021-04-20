08ae34cb7ff805a6d328b59f02dec517667891f8
hyun0k <hyunyoung9310@gmail.com>
2021-04-20 15:46:20 +0900

    Add:상품 페이지 카테고리 별 상품 데이터 전달 엔드포인트 구현 - 카테고리 id를 쿼리 파라미터로 받아서 데이터 전달 - sort 키 값이 있으면 정렬도 수행해서 데이터 전달

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.http  import JsonResponse
-from django.views import View
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q
 
 from .models import Menu, MainCategory, SubCategory, Product, ProductImage, ProductOption
 
@@ -38,6 +39,38 @@ class MainProductView(View):
                 for product in product_list
             ]
         
+            return JsonResponse({'results' : results}, status=200)
+        
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
+class ProductView(View):
+    def get(self, request):
+        try:
+            main_category_id = request.GET.get('main_category_id')
+            sub_category_id  = request.GET.get('sub_category_id')
+            sort_type        = request.GET.get('sort')
+
+            product_list     = Product.objects.filter(Q(sub_category=sub_category_id)|
+                                                      Q(main_category=main_category_id))
+
+            if(sort_type == "productPrice_asc"):
+                product_list = product_list.order_by('price')
+            
+            elif(sort_type == "productPrice_desc"):
+                product_list = product_list.order_by('-price')
+
+            results = [
+                {
+                    "id"          : product.id,
+                    "image_url"   : product.productimage_set.filter(thumbnail_status=True).first().image_url,
+                    "name"        : product.name,
+                    "description" : product.hashtag,
+                    "price"       : float(product.price), 
+                    "label"       : [{"type" : label.name, "color" : label.color} for label in product.label_set.all()]
+                }
+                for product in product_list
+            ]
             return JsonResponse({'results' : results}, status=200)
         
         except KeyError:

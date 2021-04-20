ff3788c5093c618202bcf476df1521daa84b7ae7
hyun0k <hyunyoung9310@gmail.com>
2021-04-20 15:45:08 +0900

    Add:상품 페이지 데이터 전달 엔드포인트 url 추가

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls     import path
 
-from products.views  import MenuView, MainProductView
+from products.views  import MenuView, MainProductView, ProductView
 
 urlpatterns = [
     path('/menu-bar', MenuView.as_view()),
-    path('/main', MainProductView.as_view())
+    path('/main', MainProductView.as_view()),
+    path('/category', ProductView.as_view()),
 ]

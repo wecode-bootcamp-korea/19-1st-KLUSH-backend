81cfc2ee885ef2b734b0b99dffe24cdb9aa8fdcd
hyun0k <hyunyoung9310@gmail.com>
2021-04-20 17:13:41 +0900

    Refactor:View 클래스명 수정에 따른 url 수정

--- products/urls.py
+++ products/urls.py
@@ -1,15 +1,10 @@
 from django.urls     import path
 
-from products.views  import MenuView, MainProductView, ProductView
+from products.views  import MenuView, MainProductView, ProductView, CategoryView
 
 urlpatterns = [
     path('/<int:product_id>', ProductView.as_view()),
     path('/menu-bar', MenuView.as_view()),
-<<<<<<< HEAD
     path('/main', MainProductView.as_view()),
-    path('/category', ProductView.as_view()),
+    path('/category', CategoryView.as_view()),
 ]
-=======
-    path('/main', MainProductView.as_view())
-]
->>>>>>> master

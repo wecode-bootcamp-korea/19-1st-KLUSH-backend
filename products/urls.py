d90fb83b68e22dda09104c2c1d09904cd712e47c
hyun0k <hyunyoung9310@gmail.com>
2021-04-21 19:18:21 +0900

    Add:검색 엔드포인트 추가에 따른 url 추가

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls     import path
 
-from products.views  import MenuView, MainProductView, ProductView, CategoryView
+from products.views  import MenuView, MainProductView, ProductView, CategoryView, SearchView
 
 urlpatterns = [
     path('/<int:product_id>', ProductView.as_view()),
     path('/menu-bar', MenuView.as_view()),
     path('/main', MainProductView.as_view()),
     path('/category', CategoryView.as_view()),
+    path('/search', SearchView.as_view())
 ]

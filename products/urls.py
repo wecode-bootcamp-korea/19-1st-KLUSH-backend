7b1c4d7bd069e77ce5fda4968b3ada08037b25b5
hyun0k <hyunyoung9310@gmail.com>
2021-04-22 16:08:37 +0900

    Refactor:SearchView 삭제에 따른 url삭제

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,10 @@
 from django.urls     import path
 
-from products.views  import MenuView, MainProductView, ProductView, CategoryView, SearchView
+from products.views  import MenuView, MainProductView, ProductView, CategoryView
 
 urlpatterns = [
     path('/<int:product_id>', ProductView.as_view()),
     path('/menu-bar', MenuView.as_view()),
     path('/main', MainProductView.as_view()),
     path('/category', CategoryView.as_view()),
-    path('/search', SearchView.as_view())
 ]
